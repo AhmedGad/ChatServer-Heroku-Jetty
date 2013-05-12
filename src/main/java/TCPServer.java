432b0821d521525f462575862592e05f59970e39
AhmedGad <Ahmed.mu.gad@gmail.com>
2013-05-12 20:04:47 +0200

    test5

--- src/main/java/TCPServer.java
+++ src/main/java/TCPServer.java
@@ -1,23 +1,23 @@
-import java.io.*;
-import java.net.*;
-
 class TCPServer {
-	public static void main(String argv[]) throws Exception {
-		String clientSentence;
-		String capitalizedSentence;
-		ServerSocket welcomeSocket = new ServerSocket(6789);
-		System.out.println("test");
+	// private static ServerSocket welcomeSocket;
 
-		while (true) {
-			Socket connectionSocket = welcomeSocket.accept();
-			BufferedReader inFromClient = new BufferedReader(
-					new InputStreamReader(connectionSocket.getInputStream()));
-			DataOutputStream outToClient = new DataOutputStream(
-					connectionSocket.getOutputStream());
-			clientSentence = inFromClient.readLine();
-			System.out.println("Received: " + clientSentence);
-			capitalizedSentence = clientSentence.toUpperCase() + '\n';
-			outToClient.writeBytes(capitalizedSentence);
-		}
+	public static void main(String argv[]) throws Exception {
+		System.out.println("test");
+		// String clientSentence;
+		// String capitalizedSentence;
+		// welcomeSocket = new ServerSocket(6789);
+		// System.out.println("test");
+		//
+		// while (true) {
+		// Socket connectionSocket = welcomeSocket.accept();
+		// BufferedReader inFromClient = new BufferedReader(
+		// new InputStreamReader(connectionSocket.getInputStream()));
+		// DataOutputStream outToClient = new DataOutputStream(
+		// connectionSocket.getOutputStream());
+		// clientSentence = inFromClient.readLine();
+		// System.out.println("Received: " + clientSentence);
+		// capitalizedSentence = clientSentence.toUpperCase() + '\n';
+		// outToClient.writeBytes(capitalizedSentence);
+		// }
 	}
 }
