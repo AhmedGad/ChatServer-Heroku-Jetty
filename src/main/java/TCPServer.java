5f2cbabebe78f7dcfe12c6908c20df48cf4ee495
AhmedGad <Ahmed.mu.gad@gmail.com>
2013-05-12 20:17:53 +0200

    servlet test

--- src/main/java/TCPServer.java
+++ src/main/java/TCPServer.java
@@ -1,23 +1,41 @@
-class TCPServer {
-	// private static ServerSocket welcomeSocket;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-	public static void main(String argv[]) throws Exception {
-		System.out.println("test");
-		// String clientSentence;
-		// String capitalizedSentence;
-		// welcomeSocket = new ServerSocket(6789);
-		// System.out.println("test");
-		//
-		// while (true) {
-		// Socket connectionSocket = welcomeSocket.accept();
-		// BufferedReader inFromClient = new BufferedReader(
-		// new InputStreamReader(connectionSocket.getInputStream()));
-		// DataOutputStream outToClient = new DataOutputStream(
-		// connectionSocket.getOutputStream());
-		// clientSentence = inFromClient.readLine();
-		// System.out.println("Received: " + clientSentence);
-		// capitalizedSentence = clientSentence.toUpperCase() + '\n';
-		// outToClient.writeBytes(capitalizedSentence);
-		// }
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+class TCPServer extends HttpServlet {
+
+	private static final long serialVersionUID = -7823703173356571077L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("Hello World");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+		ServletContextHandler context = new ServletContextHandler(
+				ServletContextHandler.SESSIONS);
+		context.setContextPath("/");
+		server.setHandler(context);
+		context.addServlet(new ServletHolder(new TCPServer()), "/*");
+		server.start();
+		server.join();
 	}
 }
