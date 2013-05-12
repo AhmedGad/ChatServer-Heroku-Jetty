fd176523b8f1cb6089105d53978a464bc327cc28
AhmedGad <Ahmed.mu.gad@gmail.com>
2013-05-12 19:39:01 +0200

    test4

--- src/main/java/TCPServer.java
+++ src/main/java/TCPServer.java
@@ -1,36 +1,12 @@
 import java.io.*;
 import java.net.*;
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.*;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.*;
 
-
-
-
-public class TCPServer extends HttpServlet {
-
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		resp.getWriter().print("Hello from Java!\n");
-	}
-
+class TCPServer {
 	public static void main(String argv[]) throws Exception {
-
-		Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-		ServletContextHandler context = new ServletContextHandler(
-				ServletContextHandler.SESSIONS);
-		context.setContextPath("/");
-		server.setHandler(context);
-		context.addServlet(new ServletHolder(new TCPServer()), "/*");
-		server.start();
-		server.join();
-
 		String clientSentence;
 		String capitalizedSentence;
 		ServerSocket welcomeSocket = new ServerSocket(6789);
+		System.out.println("test");
 
 		while (true) {
 			Socket connectionSocket = welcomeSocket.accept();
