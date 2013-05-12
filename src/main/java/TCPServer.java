8afd130ec8eefa743310be83eb3961b5f391b1b3
AhmedGad <Ahmed.mu.gad@gmail.com>
2013-05-12 18:26:09 +0200

    test 3

--- src/main/java/TCPServer.java
+++ src/main/java/TCPServer.java
@@ -1,24 +1,47 @@
 import java.io.*;
 import java.net.*;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.*;
 
-public class TCPServer
-{
-   public static void main(String argv[]) throws Exception
-      {
-         String clientSentence;
-         String capitalizedSentence;
-         ServerSocket welcomeSocket = new ServerSocket(6789);
 
-         while(true)
-         {
-            Socket connectionSocket = welcomeSocket.accept();
-            BufferedReader inFromClient =
-               new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            clientSentence = inFromClient.readLine();
-            System.out.println("Received: " + clientSentence);
-            capitalizedSentence = clientSentence.toUpperCase() + '\n';
-            outToClient.writeBytes(capitalizedSentence);
-         }
-      }
+
+
+public class TCPServer extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		resp.getWriter().print("Hello from Java!\n");
+	}
+
+	public static void main(String argv[]) throws Exception {
+
+		Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+		ServletContextHandler context = new ServletContextHandler(
+				ServletContextHandler.SESSIONS);
+		context.setContextPath("/");
+		server.setHandler(context);
+		context.addServlet(new ServletHolder(new TCPServer()), "/*");
+		server.start();
+		server.join();
+
+		String clientSentence;
+		String capitalizedSentence;
+		ServerSocket welcomeSocket = new ServerSocket(6789);
+
+		while (true) {
+			Socket connectionSocket = welcomeSocket.accept();
+			BufferedReader inFromClient = new BufferedReader(
+					new InputStreamReader(connectionSocket.getInputStream()));
+			DataOutputStream outToClient = new DataOutputStream(
+					connectionSocket.getOutputStream());
+			clientSentence = inFromClient.readLine();
+			System.out.println("Received: " + clientSentence);
+			capitalizedSentence = clientSentence.toUpperCase() + '\n';
+			outToClient.writeBytes(capitalizedSentence);
+		}
+	}
 }
